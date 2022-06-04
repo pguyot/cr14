153e1df20370e68a911d6356cadc3b71c5ea1af8
Paul Guyot <pguyot@kallisys.net>
2022-06-04 16:35:40 +0200

    Lint with CI (#11)

--- examples/read_system_block.py
+++ examples/read_system_block.py
@@ -5,9 +5,9 @@ import os
 rfid = os.open("/dev/rfid0", os.O_RDWR)
 print("Waiting for a chip")
 try:
-    os.write(rfid, b'p')
+    os.write(rfid, b"p")
     packet = os.read(rfid, 9)
-    if packet[0] != ord('u'):
+    if packet[0] != ord("u"):
         print(f"Unexpected packet header {packet[0]}")
     else:
         # uid is in little endian
@@ -19,13 +19,13 @@ try:
         if uid_be[0] != 0xD0:
             print(f"Unexpected MSB, got {uid_be[0]}")
         if uid_be[1] != 0x02:
-            print(f"Not a STMicroelectronics chip, will read block 255 anyway")
-        os.write(rfid, b'r' + uid_le + b'\xFF')
+            print("Not a STMicroelectronics chip, will read block 255 anyway")
+        os.write(rfid, b"r" + uid_le + b"\xFF")
         packet = os.read(rfid, 1)
-        while packet == b'u':
+        while packet == b"u":
             os.read(rfid, 8)
             packet = os.read(rfid, 1)
-        if packet == b'r':
+        if packet == b"r":
             data = os.read(rfid, 4)
             data = bytearray(data)
             data.reverse()
@@ -33,15 +33,15 @@ try:
             print(f"Data (read single): {data_str}")
         else:
             print(f"Unexpected packet header {packet[0]}")
-        os.write(rfid, b'R' + uid_le + b'\x01\xFF')
+        os.write(rfid, b"R" + uid_le + b"\x01\xFF")
         packet = os.read(rfid, 1)
-        while packet == b'u':
+        while packet == b"u":
             os.read(rfid, 8)
             packet = os.read(rfid, 1)
-        if packet == b'R':
+        if packet == b"R":
             count = os.read(rfid, 1)
             if count[0] != 1:
-                print(f"Unexpected count, got {count}, expected 1")
+                print(f"Unexpected count, got {count[0]}, expected 1")
             data = os.read(rfid, 4)
             data = bytearray(data)
             data.reverse()
--- examples/simple_uid_reader.py
+++ examples/simple_uid_reader.py
@@ -4,40 +4,76 @@ import os
 
 MODELS = {
     0x02: {
-        (6, 0b000011, 'SRIX4K'),        # http://www.orangetags.com/wp-content/downloads/datasheet/STM/srix4k.pdf
-        (6, 0b000110, 'SRI512'),        # http://www.advanide.com/wp-content/uploads/products/rfid/SRI512.pdf
-        (6, 0b001100, 'SRT512'),        # https://www.advanide.de/wp-content/uploads/products/rfid/SRT512.pdf
-        (6, 0b000111, 'SRI4K'),         # https://www.advanide.de/wp-content/uploads/products/rfid/SRI4K.pdf
-        (6, 0b001111, 'SRI2K'),         # https://www.advanide.de/wp-content/uploads/products/rfid/SRI2K.pdf
-        (8, 0x1B, 'ST25TB512-AC'),      # https://www.st.com/resource/en/datasheet/st25tb512-ac.pdf
-        (8, 0x1F, 'ST25TB04K'),         # https://www.st.com/resource/en/datasheet/st25tb04k.pdf
-        (8, 0x33, 'ST25TB512-AT'),      # https://www.st.com/resource/en/datasheet/st25tb512-at.pdf
-        (8, 0x3F, 'ST25TB02K'),         # https://www.st.com/resource/en/datasheet/st25tb02k.pdf
+        (
+            6,
+            0b000011,
+            "SRIX4K",
+        ),  # http://www.orangetags.com/wp-content/downloads/datasheet/STM/srix4k.pdf
+        (
+            6,
+            0b000110,
+            "SRI512",
+        ),  # http://www.advanide.com/wp-content/uploads/products/rfid/SRI512.pdf
+        (
+            6,
+            0b001100,
+            "SRT512",
+        ),  # https://www.advanide.de/wp-content/uploads/products/rfid/SRT512.pdf
+        (
+            6,
+            0b000111,
+            "SRI4K",
+        ),  # https://www.advanide.de/wp-content/uploads/products/rfid/SRI4K.pdf
+        (
+            6,
+            0b001111,
+            "SRI2K",
+        ),  # https://www.advanide.de/wp-content/uploads/products/rfid/SRI2K.pdf
+        (
+            8,
+            0x1B,
+            "ST25TB512-AC",
+        ),  # https://www.st.com/resource/en/datasheet/st25tb512-ac.pdf
+        (
+            8,
+            0x1F,
+            "ST25TB04K",
+        ),  # https://www.st.com/resource/en/datasheet/st25tb04k.pdf
+        (
+            8,
+            0x33,
+            "ST25TB512-AT",
+        ),  # https://www.st.com/resource/en/datasheet/st25tb512-at.pdf
+        (
+            8,
+            0x3F,
+            "ST25TB02K",
+        ),  # https://www.st.com/resource/en/datasheet/st25tb02k.pdf
     }
 }
 MANUFACTURERS = {
-   0x01: "Motorola",
-   0x02: "ST Microelectronics",
-   0x03: "Hitachi",
-   0x04: "NXP Semiconductors",
-   0x05: "Infineon Technologies",
-   0x06: "Cylinc",
-   0x07: "Texas Instruments Tag-it",
-   0x08: "Fujitsu Limited",
-   0x09: "Matsushita Electric Industrial",
-   0x0A: "NEC",
-   0x0B: "Oki Electric",
-   0x0C: "Toshiba",
-   0x0D: "Mitsubishi Electric",
-   0x0E: "Samsung Electronics",
-   0x0F: "Hyundai Electronics",
-   0x10: "LG Semiconductors",
-   0x16: "EM Microelectronic-Marin",
-   0x1F: "Melexis",
-   0x2B: "Maxim",
-   0x33: "AMIC",
-   0x44: "GenTag, Inc (USA)",
-   0x45: "Invengo Information Technology Co.Ltd",
+    0x01: "Motorola",
+    0x02: "ST Microelectronics",
+    0x03: "Hitachi",
+    0x04: "NXP Semiconductors",
+    0x05: "Infineon Technologies",
+    0x06: "Cylinc",
+    0x07: "Texas Instruments Tag-it",
+    0x08: "Fujitsu Limited",
+    0x09: "Matsushita Electric Industrial",
+    0x0A: "NEC",
+    0x0B: "Oki Electric",
+    0x0C: "Toshiba",
+    0x0D: "Mitsubishi Electric",
+    0x0E: "Samsung Electronics",
+    0x0F: "Hyundai Electronics",
+    0x10: "LG Semiconductors",
+    0x16: "EM Microelectronic-Marin",
+    0x1F: "Melexis",
+    0x2B: "Maxim",
+    0x33: "AMIC",
+    0x44: "GenTag, Inc (USA)",
+    0x45: "Invengo Information Technology Co.Ltd",
 }
 
 rfid = os.open("/dev/rfid0", os.O_RDONLY)
@@ -45,7 +81,7 @@ print("Exit with control-C")
 while True:
     try:
         packet = os.read(rfid, 9)
-        if packet[0] != ord('u'):
+        if packet[0] != ord("u"):
             print(f"Unexpected packet header {packet[0]}")
             break
         # uid is in little endian
@@ -70,7 +106,7 @@ while True:
                 if model == model_id:
                     print(f"Model: {model_str}")
                     if bits < 8:
-                        uid[2] = uid[2] &~ (model << (8 - bits))
+                        uid[2] = uid[2] & ~(model << (8 - bits))
                     else:
                         serial_start = 3
                     model_unknown = False
@@ -82,4 +118,3 @@ while True:
     except KeyboardInterrupt:
         break
 os.close(rfid)
-
